6000e3f8b6fe7c78927146177fd937c7091a220f
AiknrMacbookAir <Aiknr@HAHA>
2024-09-13 17:29:30 +0800

    fix print_pagetable by remove '0x'

--- kernel/exec.c
+++ kernel/exec.c
@@ -115,8 +115,12 @@ exec(char *path, char **argv)
   p->trapframe->epc = elf.entry;  // initial program counter = main
   p->trapframe->sp = sp; // initial stack pointer
   proc_freepagetable(oldpagetable, oldsz);
+  // printf("pid %d %s\n", p->pid, path);
   if(p->pid==1)
+  {
     vmprint(p->pagetable);
+  }
+
   return argc; // this ends up in a0, the first argument to main(argc, argv)
 
  bad:
